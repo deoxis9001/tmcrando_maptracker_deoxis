0dd4ced57fe80a36b5a65987c4a7bcb4634c8a7e
deoxis9001 <deoxis9001@pokemon-triforce.ze.cx>
2021-03-07 23:24:32 +0100

    Update Setting weekly 4 sword Remove add to error "fusion setting" Add sword canDamage_off ...

--- scripts/logic_common.lua
+++ scripts/logic_common.lua
@@ -14,7 +14,9 @@ function canDamage()
   end
 end
 function canDamage_off()
-  if Tracker:ProviderCountForCode("bow") > 0 then
+  if Tracker:ProviderCountForCode("sword") > 0 then
+    return 1
+  elseif Tracker:ProviderCountForCode("bow") > 0 then
     return 1
   elseif Tracker:ProviderCountForCode("lights") > 0 then
     return 1
