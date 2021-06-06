75395665fdce96c96979738da944da4f9602f0ff
deoxis9001 <deoxis9001@pokemon-triforce.ze.cx>
2021-06-06 09:50:07 +0200

    Update Logic DWS

---
+++ scripts/Logic/Dungeons/DWS.lua
@@ -0,0 +1,7 @@
+function DwsEnter()
+    if (has("jabber") or has("flippers")) and (has("sword") or has("bombs") or has("lights") or has("lamp") or has("ocarina")) then
+        return 1
+    else
+        return 0
+    end
+end
