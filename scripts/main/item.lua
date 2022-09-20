fe400ce988af49eb634e088e9681c07843ba5964
deoxis9001 <deoxis9001@users.noreply.github.com>
2022-09-20 14:00:35 +0200

    Add Version beta and update autotracking debug

--- scripts/main/item.lua
+++ scripts/main/item.lua
@@ -2,3 +2,6 @@ Tracker:AddItems("items/common.json")
 Tracker:AddItems("items/dungeon_items.json")
 Tracker:AddItems("items/keys.json")
 Tracker:AddItems("items/options.json")
+if(VERSION_BETA==true) then
+	Tracker:AddItems("items/beta.json")
+end 
--- scripts/settings/settings.lua
+++ scripts/settings/settings.lua
@@ -3,3 +3,5 @@
 ------------------------------------------------------------------
 AUTOTRACKER_ENABLE_ITEM_TRACKING = true
 AUTOTRACKER_ENABLE_LOCATION_TRACKING = true
+VERSION_BETA = true
+TMC_AUTOTRACKER_DEBUG = VERSION_BETA
