a6b51c53f3846efe03b54ec1d612c4df3f9e69c0
deoxis9001 <deoxis9001@users.noreply.github.com>
2023-05-27 21:36:56 +0200

    restream version

--- scripts/main/broadcast.lua
+++
@@ -1,7 +0,0 @@
-	Tracker:AddLayouts("layouts/tracker.json")
-	
-if (string.find(Tracker.ActiveVariantUID, "_h")) then
-    Tracker:AddLayouts("layouts/standard_h_broadcast.json")
-else
-    Tracker:AddLayouts("layouts/standard_broadcast.json")
-end
--- scripts/main/item.lua
+++
@@ -1,4 +0,0 @@
-Tracker:AddItems("items/common.json")
-Tracker:AddItems("items/dungeon_items.json")
-Tracker:AddItems("items/keys.json")
-Tracker:AddItems("items/options.json")
