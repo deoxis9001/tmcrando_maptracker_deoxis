996f1ebe9e3a1b8f742b26a99fda20b4fa556612
deoxis9001 <deoxis9001@users.noreply.github.com>
2024-11-09 15:26:18 +0100

    fix reverse

--- scripts/init.lua
+++ scripts/init.lua
@@ -5,5 +5,10 @@ Tracker:AddItems("items/common.json")
 Tracker:AddItems("items/dungeon_items.json")
 Tracker:AddItems("items/keys.json")
 Tracker:AddItems("items/options.json")
-Tracker:AddLayouts("layouts/tracker.json")
-Tracker:AddLayouts("layouts/standard_broadcast.json")
+if (string.find(Tracker.ActiveVariantUID, "reverse")) then
+	Tracker:AddLayouts("reverse/tracker.json")
+	Tracker:AddLayouts("reverse/standard_broadcast.json")
+else
+	Tracker:AddLayouts("layouts/tracker.json")
+	Tracker:AddLayouts("layouts/standard_broadcast.json")
+end
