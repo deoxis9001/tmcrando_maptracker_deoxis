3885512a1ad8bdb8458ed841677f03af2d4f2514
deoxis9001 <deoxis9001@users.noreply.github.com>
2025-03-12 19:06:22 +0100

    fix var r

--- scripts/logic/common/Common.lua
+++ scripts/logic/common/Common.lua
@@ -614,7 +614,6 @@ function FusionsGreenNumber(code)
 		return 1
 	end
 	if fusiongreencombined:getActive() then
-		r
 		local count_Fuser =
 			function_Cached("FusionsGreenCNumber") + function_Cached("FusionsGreenGNumber") +
 			function_Cached("FusionsGreenPNumber")
