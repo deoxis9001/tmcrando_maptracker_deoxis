dfe159133508c8090e1f206be58e33b259329d30
deoxis9001 <deoxis9001@users.noreply.github.com>
2025-04-15 04:32:33 +0200

    add map

--- pop/scripts/autotracking/events_mapping.lua
+++ pop/scripts/autotracking/events_mapping.lua
@@ -1,18 +1,18 @@
 -- Do NOT remove comments for events without hosted items, or poptracker will fail to load any events
 
 EVENTS_FLAG_MAPPING = {
-{"scroll_spin","spinattack"},
-{"scroll_fast_spin","fastspin"},
-{"scroll_fast_split","fastsplit"},
-{"scroll_great_spin","greatspin"},
-{"scroll_long_spin","longspin"},
-{"fuse_01","@Clouds - Top Right Fusion/Fusion"},
-{"fuse_02","@Clouds - Bottom Left Fusion/Fusion"},
-{"fuse_03","@Clouds - Top Left Fusion/Fusion"},
-{"fuse_04","@Clouds - Central Fusion/Fusion"},
-{"fuse_05","@Clouds - Bottom Right Fusion/Fusion"},
-{"fuse_06","@Castor Wilds - Fusions/Fusions"},
-{"fuse_07","@Castor Wilds - Fusions/Fusions"},
-{"fuse_08","@Castor Wilds - Fusions/Fusions"},
-{"fuse_09","@Veil Falls - Source of the Flow Cave/Fusion"}
+    {"scroll_spin","spinattack"},
+    {"scroll_fast_spin","fastspin"},
+    {"scroll_fast_split","fastsplit"},
+    {"scroll_great_spin","greatspin"},
+    {"scroll_long_spin","longspin"},
+    {"fuse_01","@Clouds - Top Right Fusion/Fusion"},
+    {"fuse_02","@Clouds - Bottom Left Fusion/Fusion"},
+    {"fuse_03","@Clouds - Top Left Fusion/Fusion"},
+    {"fuse_04","@Clouds - Central Fusion/Fusion"},
+    {"fuse_05","@Clouds - Bottom Right Fusion/Fusion"},
+    {"fuse_06","@Castor Wilds - Fusions/Fusions"},
+    {"fuse_07","@Castor Wilds - Fusions/Fusions"},
+    {"fuse_08","@Castor Wilds - Fusions/Fusions"},
+    {"fuse_09","@Veil Falls - Source of the Flow Cave/Fusion"}
 }
