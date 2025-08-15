3e9f86528ddfa132e1081eb45ec680dd9a5c81c9
deoxis9001 <deoxis9001@users.noreply.github.com>
2025-08-15 22:34:17 +0200

    fix setting

--- pop/scripts/autotracking/slots_data_mapping.lua
+++ pop/scripts/autotracking/slots_data_mapping.lua
@@ -1,9 +1,9 @@
 SLOTS_DATA_MAPPING = {
     -- goal_vaati
-    ["goal_vaati_dhc"]  = {"dhc_closed","OPT",{3,0,2,1,4}},
+    ["goal_vaati_dhc"]  = {"dhc_closed","OPT",{3,0,2,1,4,4}},
     -- Dungeons
-    ["dungeon_small_keys"]   =   {"small_key_none","OPT",{0,0,1,2,2,2,2,2,3,2}},
-    ["dungeon_big_keys"]   =   {"big_key_none","OPT",{0,0,1,2,2,2,2,2,3,2}},
+    ["dungeon_small_keys"]   =   {"small_key_none","OPT",{0,0,1,2,2,2,2,3,2}},
+    ["dungeon_big_keys"]   =   {"big_key_none","OPT",{0,0,1,2,2,2,2,3,2}},
    -- ["dungeon_maps"]   =   {"","OPT",{0,0,1,2,2,2,2,2,3,2}},
    -- ["dungeon_compasses"]   =   {"","OPT",{0,0,1,2,2,2,2,2,3,2}},
    -- options
