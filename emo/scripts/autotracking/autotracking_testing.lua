992a8507712c5b2104823fdd7b5364ffbe6259f4
deoxis9001 <deoxis9001@users.noreply.github.com>
2026-01-18 22:00:20 +0100

    Fix hearts

--- emo/scripts/autotracking/autotracking_testing.lua
+++ emo/scripts/autotracking/autotracking_testing.lua
@@ -1727,7 +1727,7 @@ end
 function updateHearts(segment, address)
 	local item = Tracker:FindObjectForCode("hearts")
 	if item then
-		item.CurrentStage = ReadU8(segment, address) / 8 - 3
+		item.CurrentStage = ReadU8(segment, address) / 8 - 1
 	end
 end
 function updateWarps(segment, type)
